c213dccccc399e54c18283e74ad6329576556907
Jari Bakken <jari@holderdeord.no>
2017-08-05 14:19:17 +0200

    Status by dep

---
+++ indexer/status-by-dep.js
@@ -0,0 +1,75 @@
+const Promise = require('bluebird');
+const elasticsearch = require('elasticsearch');
+const csv = require('csv');
+const { keyBy } = require('lodash');
+
+const es = new elasticsearch.Client({
+    host: process.env.ELASTICSEARCH_URL || 'localhost:9200',
+    version: process.env.ELASTICSEARCH_VERSION || '5.4',
+    log: 'warning',
+    defer: () => Promise.defer()
+});
+
+es
+    .search({
+        index: 'hdo-promise-tracker-2017',
+        type: 'promise',
+        body: {
+            query: {
+                query_string: {
+                    query: '*'
+                }
+            },
+            aggs: {
+                ministries: {
+                    terms: {
+                        field: 'ministry',
+                        size: 100
+                    },
+                    aggs: {
+                        statuses: {
+                            terms: {
+                                field: 'status'
+                            }
+                        }
+                    }
+                }
+            },
+            size: 0
+        }
+    })
+    .then(res => {
+        const out = csv.stringify({ delimiter: '\t' });
+        out.pipe(process.stdout);
+
+        out.write([
+            'Departement',
+            'Holdt',
+            'Holdt %',
+            'Delvis holdt',
+            'Delvis holdt %',
+            'Brutt',
+            'Brutt %',
+            'Kan ikke etterprøves',
+            'Kan ikke etterprøves %',
+            'Totalt'
+        ]);
+
+        res.aggregations.ministries.buckets.forEach(bucket => {
+            const statuses = keyBy(bucket.statuses.buckets, 'key');
+            out.write([
+                bucket.key,
+                statuses.kept ? statuses.kept.doc_count : 0,
+                statuses.kept ? statuses.kept.doc_count * 100 / bucket.doc_count : 0,
+                statuses['partially-kept'] ? statuses['partially-kept'].doc_count : 0,
+                statuses['partially-kept'] ? statuses['partially-kept'].doc_count * 100 / bucket.doc_count : 0,
+                statuses.broken ? statuses.broken.doc_count : 0,
+                statuses.broken ? statuses.broken.doc_count * 100 / bucket.doc_count : 0,
+                statuses.uncheckable ? statuses.uncheckable.doc_count : 0,
+                statuses.uncheckable ? statuses.uncheckable.doc_count * 100 / bucket.doc_count : 0,
+                bucket.doc_count
+            ]);
+        });
+
+        out.end();
+    });
