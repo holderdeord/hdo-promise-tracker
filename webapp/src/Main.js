ef4365f6afd81b0aa8590adb148dbc920a1f5d95
Jari Bakken <jari@holderdeord.no>
2017-08-07 09:20:29 +0200

    Revert "text-balancer"
    This reverts commit ca365b910df9f77a274d90363c2146dbbfc36596.

--- webapp/src/Main.js
+++ webapp/src/Main.js
@@ -5,8 +5,6 @@ import PromiseList from './PromiseList';
 import SearchApi from './SearchApi';
 import StatusCharts from './StatusCharts';
 
-import textBalancer from 'text-balancer';
-
 export default class Main extends Component {
     api = new SearchApi();
 
@@ -16,12 +14,6 @@ export default class Main extends Component {
 
     componentDidMount() {
         this.api.getStats().then(stats => this.setState({stats}))
-        textBalancer.balanceText();
-    }
-
-    componentDidUpdate() {
-        textBalancer.balanceText();
-
     }
 
     render() {
