165bdce9378815676b64a0ae1841185eb701f4a3
Jari Bakken <jari@holderdeord.no>
2017-08-07 13:05:38 +0200

    intro text update

--- webapp/src/Intro.js
+++ webapp/src/Intro.js
@@ -5,7 +5,7 @@ export default ({count}) =>
         <div className="row">
             <div className="col-xs-10 col-xs-offset-1 col-md-8 col-md-offset-2">
                 <p className="lead text-xs-center">
-                    Vi har sjekket om Solberg-regjeringen har holdt løftene fra regjeringserklæringen fra 2013. Her kan du se status på alle {count ? `de ${count}` : ''} løftene og finne ut hva som har blitt gjort i perioden.
+                    Vi har sjekket alle {count ? count : ''} løftene i Sundvoldenerklæringen - plattformen som regjeringen fremforhandlet etter valget i 2013. Her kan du se status på alle løftene og finne ut hva som har blitt gjort i perioden.
                 </p>
             </div>
         </div>
