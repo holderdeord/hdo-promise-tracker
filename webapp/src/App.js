62b160a1592d8cea7f5ac4e1bb4ec95b21b9ce79
Jari Bakken <jari@holderdeord.no>
2020-02-04 13:17:03 +0100

    add note

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import Header from './Header';
-import Main from './Main';
-import RelatedServices from './RelatedServices';
-import FacebookComments from './FacebookComments';
-import Footer from './Footer';
-import qs from 'query-string'
+import Header from "./Header";
+import Main from "./Main";
+import RelatedServices from "./RelatedServices";
+import FacebookComments from "./FacebookComments";
+import Footer from "./Footer";
+import qs from "query-string";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="container">
-                <Header />
-                <Main query={qs.parse(window.location.search.slice(1))}/>
-                <RelatedServices />
-                <FacebookComments />
-                <Footer />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="container">
+        <Header />
+
+        <div className="m-t-1">
+          <div className="alert alert-info">
+            <strong>Holder de ord</strong> legges ned i løpet av 2020.
+          </div>
+        </div>
+
+        <Main query={qs.parse(window.location.search.slice(1))} />
+        <RelatedServices />
+        <FacebookComments />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
